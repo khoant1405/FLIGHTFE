64320c7442ff7d4c371e6734607c88968220a947
vothithusang <thusangvo9888@gmail.com>
2021-06-21 22:38:50 +0700

    update module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BootstrapModule } from './module/bootstrap/bootstrap.module';
 import { MaterialModule } from './module/material/material.module';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    BootstrapModule,
+    BootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
